5551d4d2d5e7ebf95ac506a7bebdec6862e4c0b5
Debb <debbsxk@gmail.com>
2024-10-28 16:32:48 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Debora Kurdzhieva on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
